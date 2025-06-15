2fd9453c236f5c906957078ad6de6b98ca549552
come3300 <suke3300@gmail.com>
2025-06-15 13:40:48 +0900

    build時のエラーを解消

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -5,17 +5,27 @@ import { buttonVariants } from "@/components/ui/button";
 import { dashboardConfig } from "@/config/dashboard";
 import { cn } from "@/libs/utils";
 import Link from "next/link";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import type { Database } from "@/libs/database.types";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <div className="flex min-h-screen flex-col space-y-6">
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex items-center justify-between py-4 h-16">
-          <MainNav items={dashboardConfig.mainNav} />
+          <MainNav items={dashboardConfig.mainNav} session={session} />
         </div>
       </header>
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
--- components/supabase-listener.tsx
+++ components/supabase-listener.tsx
@@ -8,11 +8,24 @@ import type { Database } from '@/libs/database.types'
 const SupabaseListener = async () => {
   const supabase = createServerComponentClient<Database>({ cookies })
 
+  // セッションの取得
   const {
     data: { session },
   } = await supabase.auth.getSession()
 
-  return <Navigation session={session} />
+  // プロフィールの取得
+  let profile: Database['public']['Tables']['profiles']['Row'] | null = null
+  if (session) {
+    const { data: currentProfile } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', session.user.id)
+      .single()
+
+    profile = currentProfile
+  }
+
+  return <Navigation session={session} profile={profile} />
 }
 
 export default SupabaseListener
