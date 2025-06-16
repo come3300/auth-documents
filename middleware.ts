fc738dc5fa9193c99e9a2aaf0efd3a491891c52a
come3300 <suke3300@gmail.com>
2025-06-16 16:20:48 +0900

    リダイレクト処理の挙動がおかしいためmiddlewareの内容を修正

--- middleware.ts
+++ middleware.ts
@@ -1,54 +1,33 @@
-import { getToken } from "next-auth/jwt";
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 
-import type { NextRequest } from "next/server";
-import type { Database } from "@/libs/database.types";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
 
-export default withAuth(
-  async function middleware(req) {
-    const res = NextResponse.next();
+import type { NextRequest } from 'next/server'
+import type { Database } from '@/libs/database.types'
 
-    // Supabase セッションの取得
-    const supabase = createMiddlewareClient<Database>({ req, res });
-    await supabase.auth.getSession();
-
-    const token = await getToken({ req });
-    const isAuth = !!token;
-    const isAuthPage =
-      req.nextUrl.pathname.startsWith("/login") ||
-      req.nextUrl.pathname.startsWith("/register");
-
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL("/phase-lists", req.url));
-      }
-
-      return null;
-    }
-
-    if (!isAuth) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    return res;
-  },
-  {
-    callbacks: {
-      async authorized({ req, token }) {
-        return true;
-      },
-    },
+export async function middleware(req: NextRequest) {
+ const res = NextResponse.next()
+ const supabase = createMiddlewareClient<Database>({ req, res })
+  await supabase.auth.getSession()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (req.url.includes('_next')) return;
+  if (!session && !req.url.includes('/login')) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
-);
+  if (session && req.url.includes('/login')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+ return res
+}
 
-export const config = {
-  matcher: [
-    "/dashboard/:path",
-    "/editor/:path",
-    "/login",
-    "/register",
-    "/phase-lists",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/dashboard/:path",
+//     "/editor/:path",
+//     "/login",
+//     "/register",
+//     "/phase-lists",
+//   ],
+// };
