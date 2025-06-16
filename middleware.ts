e630822d9f5142c58d27d49c3aed352d7c6298f4
come3300 <suke3300@gmail.com>
2025-06-17 04:02:59 +0900

    ログインしてないときのリダイレクト処理の調整

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
@@ -6,20 +5,33 @@ import type { NextRequest } from 'next/server'
 import type { Database } from '@/libs/database.types'
 
 export async function middleware(req: NextRequest) {
- const res = NextResponse.next()
- const supabase = createMiddlewareClient<Database>({ req, res })
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<Database>({ req, res })
   await supabase.auth.getSession()
   const {
     data: { session },
-  } = await supabase.auth.getSession();
-  if (req.url.includes('_next')) return;
-  if (!session && !req.url.includes('/login')) {
-    return NextResponse.redirect(new URL('/login', req.url));
+  } = await supabase.auth.getSession()
+
+  // `_next` のリクエストは処理しない
+  if (req.url.includes('_next')) return
+
+  // ログインしていない場合のアクセス許可
+  const publicPaths = ['/', '/login', '/register']
+  if (!session && publicPaths.some((path) => req.url.includes(path))) {
+    return res
   }
+
+  // ログインしていない場合、他のページにアクセスすると `/login` にリダイレクト
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  // ログインしている場合、`/login` や `/register` にアクセスすると `/` にリダイレクト
   if (session && req.url.includes('/login')) {
-    return NextResponse.redirect(new URL('/', req.url));
+    return NextResponse.redirect(new URL('/', req.url))
   }
- return res
+
+  return res
 }
 
 // export const config = {
